6f85b71c21aed3734b53f27a5560744c75a52ef7
Emma Nilsson <emma.n.gagen@outlook.com>
2022-08-23 14:10:59 +0200

    kalender

--- src/components/Pages/Booking.tsx
+++ src/components/Pages/Booking.tsx
@@ -1,7 +1,24 @@
+import { SetStateAction, useState } from "react";
+import DatePicker from "sassy-datepicker";
+
 export const Booking = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (newDate: SetStateAction<Date>) => {
+    console.log(`New date selected - ${newDate.toString()}`);
+    setDate(newDate);
+  };
+
   return (
-    <>
-      <h1>Hello from Booking</h1>
-    </>
+    <div className="mainBooking">
+      <h1>Här bokar du bord</h1>
+      <div className="datePicker">
+        <DatePicker
+          onChange={onChange}
+          value={date}
+          options={{ weekStartsFrom: "Monday" }}
+        />
+      </div>
+    </div>
   );
 };
