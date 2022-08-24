8b71838f8c50146a7e3c58af935b8af800b2a0cc
Emma Nilsson <emma.n.gagen@outlook.com>
2022-08-24 11:40:50 +0200

    tider visas i kalendern

--- src/components/Pages/Booking.tsx
+++ src/components/Pages/Booking.tsx
@@ -1,13 +1,22 @@
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import DatePicker from "sassy-datepicker";
 
 export const Booking = () => {
   const [date, setDate] = useState(new Date());
+  const [showTime, setShowTime] = useState(false);
 
-  const onChange = (newDate: SetStateAction<Date>) => {
+  const onChange = (newDate: Date) => {
     console.log(`New date selected - ${newDate.toString()}`);
     setDate(newDate);
+    setShowTime(true);
   };
+
+  const Results = () => (
+    <div id="results" className="search-results">
+      <p>18:00</p>
+      <p>21:00</p>
+    </div>
+  );
 
   return (
     <div className="mainBooking">
@@ -18,6 +27,7 @@ export const Booking = () => {
           value={date}
           options={{ weekStartsFrom: "Monday" }}
         />
+        {showTime ? <Results /> : null}
       </div>
     </div>
   );
