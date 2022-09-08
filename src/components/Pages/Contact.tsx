0a494b49c9b3b901be989a58d36341eac52a2cdd
Felix Canberger <felix.canberger@hotmail.com>
2022-09-08 17:42:50 +0200

    added contact

--- src/components/Pages/Contact.tsx
+++ src/components/Pages/Contact.tsx
@@ -1,7 +1,23 @@
+import googleMaps from "../../assets/mapsis.png"
 export const Contact = () => {
   return (
     <>
-      <h1>Hello from Contact</h1>
+    <div className="Contact">
+      <div className="contact-container">
+        <h2 className="contactus">Kontakta oss</h2>
+        <p>Tel: 08 - 12 34 56</p>
+        <p>Mail: info@buonamascella.se</p>
+        <p>Adress: Gott käk 10, Stockholm</p>
+
+        <h2 className="hittahit">Hitta hit</h2>
+        <img
+          className="GMaps"
+          src={googleMaps}
+          alt="Karta"
+          width={600}
+          />
+      </div>
+    </div>
     </>
   );
 };
